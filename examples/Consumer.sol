d98e780f02116dd190d4e96bcceed31f9cf527ed
Andrew Coutts <andrewjohncoutts@gmail.com>
2020-01-18 20:35:58 -0500

    Update Consumer.sol

--- examples/Consumer.sol
+++ examples/Consumer.sol
@@ -9,112 +9,35 @@ contract ATestnetConsumer is ChainlinkClient, Ownable {
   uint256 public currentPrice;
   int256 public changeDay;
   bytes32 public lastMarket;
-//   int public blockTime;
+  int public blockTime;
   
-//   event RequestBlockTimeFulfilled(
-//     bytes32 indexed requestId,
-//     int indexed time
-//   );
-
-  event RequestEthereumPriceFulfilled(
+  event RequestBlockTimeFulfilled(
     bytes32 indexed requestId,
-    uint256 indexed price
-  );
-
-  event RequestEthereumChangeFulfilled(
-    bytes32 indexed requestId,
-    int256 indexed change
-  );
-
-  event RequestEthereumLastMarket(
-    bytes32 indexed requestId,
-    bytes32 indexed market
+    int indexed time
   );
 
   constructor() public Ownable() {
     setPublicChainlinkToken();
   }
- 
   
-//   function requestBlockTime(address _oracle, string _jobId)
-//     public
-//     onlyOwner
-//   {
-//     Chainlink.Request memory req = buildChainlinkRequest(stringToBytes32(_jobId), this, this.fulfillBlockTime.selector);
-//     req.add("rpc_command", "getRawTransaction");
-//     req.add("tx_id", "96f4cfef79bedea7b389dd07dff5aaf491a8932ec7d5a2bd2d96dfbc916031ee");
-//     req.add("verbose", "true");
-//     req.add("copyPath", "tx_row.blocktime");
-//     sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
-//   }
-
-  function requestEthereumPrice(address _oracle, string _jobId)
+  function requestBlockTime(address _oracle, string _jobId, string _tx_id, string _path)
     public
     onlyOwner
   {
-    Chainlink.Request memory req = buildChainlinkRequest(stringToBytes32(_jobId), this, this.fulfillEthereumPrice.selector);
-    req.add("get", "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD");
-    req.add("path", "USD");
-    req.addInt("times", 100);
-    sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
-  }
-
-  function requestEthereumChange(address _oracle, string _jobId)
-    public
-    onlyOwner
-  {
-    Chainlink.Request memory req = buildChainlinkRequest(stringToBytes32(_jobId), this, this.fulfillEthereumChange.selector);
-    req.add("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD");
-    req.add("path", "RAW.ETH.USD.CHANGEPCTDAY");
-    req.addInt("times", 1000000000);
-    sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
-  }
-
-  function requestEthereumLastMarket(address _oracle, string _jobId)
-    public
-    onlyOwner
-  {
-    Chainlink.Request memory req = buildChainlinkRequest(stringToBytes32(_jobId), this, this.fulfillEthereumLastMarket.selector);
-    req.add("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD");
-    string[] memory path = new string[](4);
-    path[0] = "RAW";
-    path[1] = "ETH";
-    path[2] = "USD";
-    path[3] = "LASTMARKET";
-    req.addStringArray("path", path);
+    Chainlink.Request memory req = buildChainlinkRequest(stringToBytes32(_jobId), this, this.fulfillBlockTime.selector);
+    req.add("rpc_command", "getRawTransaction");
+    req.add("tx_id", _tx_id);
+    req.add("verbose", "true");
+    req.add("path", _path);
     sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
   }
   
-//   function fulfillBlockTime(bytes32 _requestId, int _block_time)
-//     public
-//     recordChainlinkFulfillment(_requestId)
-//   {
-//     emit RequestBlockTimeFulfilled(_requestId, _block_time);
-//     blockTime = _block_time;
-//   }
-
-  function fulfillEthereumPrice(bytes32 _requestId, uint256 _price)
+  function fulfillBlockTime(bytes32 _requestId, int _block_time)
     public
     recordChainlinkFulfillment(_requestId)
   {
-    emit RequestEthereumPriceFulfilled(_requestId, _price);
-    currentPrice = _price;
-  }
-
-  function fulfillEthereumChange(bytes32 _requestId, int256 _change)
-    public
-    recordChainlinkFulfillment(_requestId)
-  {
-    emit RequestEthereumChangeFulfilled(_requestId, _change);
-    changeDay = _change;
-  }
-
-  function fulfillEthereumLastMarket(bytes32 _requestId, bytes32 _market)
-    public
-    recordChainlinkFulfillment(_requestId)
-  {
-    emit RequestEthereumLastMarket(_requestId, _market);
-    lastMarket = _market;
+    emit RequestBlockTimeFulfilled(_requestId, _block_time);
+    blockTime = _block_time;
   }
 
   function getChainlinkToken() public view returns (address) {
